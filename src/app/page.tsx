d8475f06249be8f3302cfad12d845e94a6c97c53
jadeleonard <lukabartolomeo9@gmail.com>
2024-06-01 18:05:44 +0800

    added client to the main file system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
+'use client'
 import React from 'react';
 import Hero from './Components/hook/Hero';
-import { Button } from '@/components/ui/button';
+
 import Navbar from './Components/Navbar';
 
 
 const App = () => {
   return (
     <div>
-      <nav>
+   
 
     <Navbar />
 
-      </nav>
-      <main>
+  
        
       <Hero />
    
       
 
-      </main>
+    
       <footer>
         
 
